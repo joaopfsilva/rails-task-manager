267d42e767f65aa6aeb3d9cafb0120468d92c3df
joao silva <j0a0.silva.kle@gmail.com>
2017-03-21 15:20:57 +0100

    done bad layout

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+
+  before_action :not_dry, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @task = Task.new
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def edit
+  end
+
+
+  def show
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def not_dry
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:msg, :creator)
+  end
+
+end
+
+
+
+# As a user I can display the details of a task
+# As a user I can add a task in my ToDo list
+# As a user I can edit the details of a task
+# As a user I can remove a task from my ToDo list
